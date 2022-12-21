769e1a0cfcc05e043e0beacc4ba3d147db29c93b
jiniux <53437303+jiniux@users.noreply.github.com>
2022-12-21 14:49:56 +0100

    Ensure database creation on Program.cs

--- Osekai.Octon/Program.cs
+++ Osekai.Octon/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MySqlOsekaiDbContext>(options => 
     options.UseMySql(builder.Configuration.GetConnectionString("MySql")!, MySqlServerVersion.LatestSupportedServerVersion));
 builder.Services.AddMemoryCache();
+
 #if DEBUG
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 #else
@@ -16,10 +17,14 @@ builder.Services.AddRazorPages();
 builder.Services.AddRouting();
 
 var app = builder.Build();
-
 app.UseRouting();
 
 app.MapRazorPages();
 app.MapControllers();
+
+IServiceScope scope = app.Services.CreateScope();
+
+await using (var context = scope.ServiceProvider.GetService<MySqlOsekaiDbContext>()!)
+    await context.Database.MigrateAsync();
 
 await app.RunAsync();
