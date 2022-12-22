b7f3c859868297744c58b6377877f43d3ba5e2b2
jiniux <53437303+jiniux@users.noreply.github.com>
2022-12-22 13:02:55 +0100

    Refactor ambiguous methods name in PathUtils.cs

--- Osekai.Octon/PathUtils.cs
+++ Osekai.Octon/PathUtils.cs
@@ -2,17 +2,17 @@
 
 public static class PathUtils
 {
-    public static string AppendQueryVersion(string @string) => @string + "?v=" + Constants.Version;
+    public static string AppendVersionQuery(string @string) => @string + "?v=" + Constants.Version;
 
     private static readonly Dictionary<string, string> AppendQueryVersionResultCache = new Dictionary<string, string>(); 
 
-    public static string AppendQueryVersionCached(string @string)
+    public static string AppendVersionQueryCached(string @string)
     {
         string? result;
         if (AppendQueryVersionResultCache.TryGetValue(@string, out result))
             return result;
 
-        result = AppendQueryVersion(@string);
+        result = AppendVersionQuery(@string);
         AppendQueryVersionResultCache.Add(@string, result);
 
         return result;
