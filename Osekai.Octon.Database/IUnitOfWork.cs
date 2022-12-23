7ba7121778737694dcbf40e8c10cf58eb0707068
jiniux <53437303+jiniux@users.noreply.github.com>
2022-12-23 11:31:29 +0100

    Remove transaction concern from IUnitOfWork

--- Osekai.Octon.Database/IUnitOfWork.cs
+++ Osekai.Octon.Database/IUnitOfWork.cs
@@ -3,16 +3,9 @@ using Osekai.Octon.Database.Repositories;
 
 namespace Osekai.Octon.Database;
 
-public interface IUnitOfWork: IAsyncDisposable, IDisposable
+public interface IUnitOfWork
 {
     public IAppRepository AppRepository { get; }
-
-    Task BeginTransactionAsync(
-        IsolationLevel isolationLevel = IsolationLevel.Serializable, 
-        CancellationToken cancellationToken = default);
-
-    Task CommitTransactionAsync(CancellationToken cancellationToken = default);
-    Task RollbackTransactionAsync(CancellationToken cancellationToken = default);
     
     Task SaveAsync(CancellationToken cancellationToken = default);
 }
