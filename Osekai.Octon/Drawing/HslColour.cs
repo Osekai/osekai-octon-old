3bc317be9554abb46cfcf42b67b6285e0752701f
jiniux <53437303+jiniux@users.noreply.github.com>
2023-02-22 11:50:02 +0100

    Add equality methods to HslColour and RgbColour.

--- Osekai.Octon/Drawing/HslColour.cs
+++ Osekai.Octon/Drawing/HslColour.cs
@@ -1,6 +1,6 @@
 ﻿namespace Osekai.Octon.Drawing;
 
-public readonly struct HslColour
+public readonly struct HslColour: IEquatable<HslColour>
 {
     public HslColour(double h, double s, double l)
     {
@@ -12,4 +12,29 @@ public readonly struct HslColour
     public double H { get; }
     public double S { get; }
     public double L { get; }
+
+    public bool Equals(HslColour other)
+    {
+        return H.Equals(other.H) && S.Equals(other.S) && L.Equals(other.L);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is HslColour other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(H, S, L);
+    }
+
+    public static bool operator ==(HslColour left, HslColour right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(HslColour left, HslColour right)
+    {
+        return !left.Equals(right);
+    }
 }
--- Osekai.Octon/Drawing/RgbColour.cs
+++ Osekai.Octon/Drawing/RgbColour.cs
@@ -1,6 +1,6 @@
 ﻿namespace Osekai.Octon.Drawing;
 
-public readonly struct RgbColour
+public readonly struct RgbColour: IEquatable<RgbColour>
 {
     public RgbColour(byte r, byte g, byte b)
     {
@@ -12,4 +12,29 @@ public readonly struct RgbColour
     public byte R { get; }
     public byte G { get; }
     public byte B { get; }
+
+    public bool Equals(RgbColour other)
+    {
+        return R == other.R && G == other.G && B == other.B;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is RgbColour other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(R, G, B);
+    }
+
+    public static bool operator ==(RgbColour left, RgbColour right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(RgbColour left, RgbColour right)
+    {
+        return !left.Equals(right);
+    }
 }
